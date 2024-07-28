03df40edebdac8d94c7fbe48de5becad28810e1c
myz <2811651421@qq.com>
2024-07-28 16:41:56 +0800

    修改抽奖背景样式

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,33 @@
 </template>
 
 <style>
+body {
+  /* ... 其他样式 ... */
+
+  /* 定义滚动条的整体样式 */
+
+  &::-webkit-scrollbar {
+    width: 5px; /* 滚动条的宽度 */
+  }
+
+  /* 定义滚动条轨道的样式 */
+
+  &::-webkit-scrollbar-track {
+    background-color: rgb(236, 204, 217); /* 轨道背景颜色 */
+    border-radius: 6px; /* 轨道圆角 */
+  }
+
+  /* 定义滚动条滑块的样式 */
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #ecccd9; /* 滑块颜色 */
+    border-radius: 6px; /* 滑块圆角 */
+  }
+
+  /* 当鼠标悬停在滑块上时的样式 */
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #d54978; /* 悬停时滑块的颜色 */
+  }
+}
 </style>
--- src/views/ScratchOffDrawView.vue
+++ src/views/ScratchOffDrawView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="center-container">
     <span>刮刮乐</span>
   </div>
 </template>
@@ -8,6 +8,18 @@
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.center-container {
+  /* 设置容器高度为视口高度的100% */
+  height: 100vh;
+  /* 背景图片不重复且填充整个容器 */
+  background-image: url("@/assets/backgroud.png");
+  background-size: cover; /* 图片将被拉伸以填充整个容器 */
+  background-position: center; /* 图片定位在中心 */
+  background-repeat: no-repeat; /* 确保图片不重复 */
+  /* 使内部元素水平和垂直居中 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
